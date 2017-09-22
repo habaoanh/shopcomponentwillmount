513b0e16c94402a0546deb84da75b6701296e081
habaoanh <habaoanh@gmail.com>
2017-09-21 20:35:26 -0700

    finish lesson 11

--- src/components/Shop/Authentication/SignUp.js
+++ src/components/Shop/Authentication/SignUp.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
-import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import register from '../../api/register';
+import { View, TextInput, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import register from '../../../api/register';
 
 export default class SignUp extends Component {
     constructor(props) {
@@ -19,9 +19,35 @@ export default class SignUp extends Component {
         register(email, name, password)
         .then(res => {
             console.log(res);
+            if (res === 'THANH_CONG') return this.onSuccess();
+            this.onFail();
         });
     }
+    removeEmail() {
+        this.setState({ email: '' });
+    }
 
+    onSuccess() {
+        Alert.alert(
+            'Notice',
+            'Sign up successfully',
+            [
+                { text: 'OK', onPress: this.removeEmail.bind(this) }
+            ],
+            { cancelable: false }
+        );
+    }
+
+    onFail() {
+        Alert.alert(
+            'Notice',
+            'Email has been used by other',
+            [
+                { text: 'OK', onPress: () => console.log('Ask me later pressed') }
+            ],
+            { cancelable: false }
+        );
+    }
     render() {
         const { inputStyle, bigButton, buttonText } = styles;
         return (
